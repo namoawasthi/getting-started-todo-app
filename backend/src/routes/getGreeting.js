8f74380715fe5f9e343bf6ef6fd8b409fbd4856c
awasthinamo <awasthinamo@gmail.com>
2024-06-24 16:05:33 +0530

    greetings change 3

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -6,6 +6,6 @@ const GREETINGS = [
 
 module.exports = async (req, res) => {
     res.send({
-        greetings: GREETINGS[Math.floor(Math.random() * GREETINGS.length)],
+        greetings: GREETINGS[Math.floor( Math.random() * GREETINGS.length )],
     });
 };
